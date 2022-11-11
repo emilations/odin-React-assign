029f6c46cce32dfa2627bbd0bc87b614a69c6f5b
Emile Mouannes <emile.mouannes@outlook.com>
2022-11-11 17:33:15 -0500

    Add list functionality

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -5,17 +5,38 @@ class Input extends Component {
     super(props);
     this.state = {
       list: ["Cook the bear", "Turn off the light"],
+      current: "",
     };
+    this.addInputValue = this.addInputValue.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
   }
+
+  addInputValue() {
+    let newArray = [...this.state.list]
+    newArray.push(this.state.current)
+    console.log(newArray)
+    this.setState({
+      list: [...newArray],
+    })
+  }
+
+  updateInputValue(evt) {
+    const val = evt.target.value;
+    this.setState({
+      current: val,
+    });
+  }
+
   render() {
     return (
       <div>
         <ul>
-          {/* {(console.log(this.state.list))} */}
-          <li>eww</li>
+          {this.state.list.map((elem) => {
+            return <li>{elem}</li>;
+          })}
         </ul>
-        <input type="text" />
-        <button>Submit</button>
+        <input type="text" onChange={this.updateInputValue}></input>
+        <button onClick={this.addInputValue}>Submit</button>
       </div>
     );
   }
